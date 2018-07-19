e888f88bc59dbbcf162cd0eb66b3c79d12c4fbf1
Nikola <nikolabojkovic@gmail.comm>
2018-07-19 20:56:05 +0200

    Initial Commit

---
+++ PizzaDelivery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PizzaDelivery
+//
+//  Created by Nikola Bojkovic on 7/19/18.
+//  Copyright © 2018 Nikola Bojkovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
